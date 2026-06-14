c769ee526a778ce05428e67e03250a32ce90cf08
HarshPandey81 <harshpandeyh12@gmail.com>
2026-06-14 14:11:37 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ascii{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.nextInt();
+        System.out.print((char)ch);
+    }
+}
